4baf51a1e2a8c7b27943bfd212358e29676f5007
JackLovel <gogkatsu@outlook.com>
2020-11-17 11:31:02 +0800

    add model

---
+++ BlogSystem.Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogSystem.Models
+{
+    public class User : BaseEntity
+    {
+        [Required,StringLength(40),Column(TypeName = "varchar")]
+        public string Email { get; set; }        
+        [Required,StringLength(30),Column(TypeName = "varchar")]
+        public string Password { get; set; }
+        [Required, StringLength(300), Column(TypeName = "varchar")]
+        public string ImagePath { get; set; }
+        /// <summary>
+        /// 粉丝数量
+        /// </summary>
+        public int FunsCount { get; set; }
+        /// <summary>
+        /// 关注数
+        /// </summary>
+        public int FocusCount { get; set; }
+        /// <summary>
+        /// 博客名
+        /// </summary>
+        public string SiteName { get; set; }
+    }
+}
